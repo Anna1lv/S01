9dbd663f0b3ac81a915376ff3a146feac3a42ce1
Anna Livia de Souza Pereira <annalv022@gmail.com>
2026-08-30 15:41:43 -0300

    Exercicio 1
    Add a function to validate vehicle license plates based on character and number counts.

--- Relatório 3/exercicio-1.rs
+++ Relatório 3/exercicio-1.rs
@@ -1,1 +1,38 @@
+use std::io;
 
+fn validar_placa(placa: &str) -> bool {
+    if placa.len() < 7 {
+        return false;
+    }
+
+    let mut maiusculas = 0;
+    let mut numeros = 0;
+
+    for c in placa.chars() {
+        if c.is_ascii_uppercase() {
+            maiusculas = maiusculas + 1;
+        }
+        if c.is_numeric() {
+            numeros = numeros + 1;
+        }
+    }
+
+    maiusculas >= 4 && numeros >= 2
+    // return
+}
+
+fn main() {
+    loop {
+        let mut placa = String::new();
+        //nao aceita acentos
+        println!("Digite a placa do veiculo:");
+        io::stdin().read_line(&mut placa).expect("Erro ao ler");
+
+        if validar_placa(placa.trim()) { //se é true entao entra
+            println!("Placa cadastrada no sistema!");
+            break; 
+        } else {
+            println!("Placa invalida. Tente novamente!");
+        }
+    }
+}
